2eb65d360c8feed80c29d30a410db7d1c116a554
guitarisprettierthanpiano <shfabrizio@gmail.com>
2022-05-12 22:00:17 -0400

    adding client folder with html bp

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ const path = require('path');
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json()); //req.body
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client')));
-// }
+//req.body
+app.use(express.json()); 
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 const recentRoutes = require('./server/routes/routes.js');
 app.use('/recent', recentRoutes);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Up on port ${PORT}.`);
+  // console.log(`Up on port ${PORT}.`);
 });
